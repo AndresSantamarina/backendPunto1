59b6e1d0ae1201beb01460812c9505ea0bd83ad6
Andres Santamarina <andresesantamarina@gmail.com>
2024-03-15 18:18:21 -0300

    agregue la logica para editar una tarea

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -24,7 +24,7 @@ export const obtenerTareaPorId = async (req, res) => {
 
 export const obtenerTareaPorNombre = async (req, res) => {
     try {
-        const tareaBuscadaPorNombre = await Tarea.find({nombreTarea: req.params.nombre}).exec();
+        const tareaBuscadaPorNombre = await Tarea.find({ nombreTarea: req.params.nombre }).exec();
         console.log(req.params.nombre)
         res.status(200).json(tareaBuscadaPorNombre)
     } catch (error) {
@@ -47,5 +47,22 @@ export const crearTarea = async (req, res) => {
         res.status(400).json({
             mensaje: "No se pudo procesar la solicitud de crear tarea"
         })
+    }
+}
+
+export const editarTarea = async (req, res) => {
+    try {
+        const buscarTarea = await Tarea.findById(req.params.id)
+        if(!buscarTarea){
+            return res.status(404).json({
+                mensaje: "No se puedo editar la tarea, el id es incorrecto"
+            })
+        }
+        await Tarea.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({
+            mensaje: "El producto fue modificado exitosamente"
+        })
+    } catch (error) {
+        res.status(500).json({mensaje: "Ocurrió un error al intentar editar la tarea"})
     }
 }
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { crearTarea, listarTareas, obtenerTareaPorId, obtenerTareaPorNombre } from "../controllers/tareas.controllers.js";
+import { crearTarea, editarTarea, listarTareas, obtenerTareaPorId, obtenerTareaPorNombre } from "../controllers/tareas.controllers.js";
 
 const router = Router();
 
 router.route('/tareas').get(listarTareas).post(crearTarea)
-router.route('/tarea/:id').get(obtenerTareaPorId)
+router.route('/tarea/:id').get(obtenerTareaPorId).put(editarTarea)
 router.route('/tarea/nombre/:nombre').get(obtenerTareaPorNombre)
 
 export default router;
